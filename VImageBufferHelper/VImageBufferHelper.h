84b83a3c0c8d728f8c24b8f1b0f20536c772927d
Alessandro Toschi <aletos@plexagon.it>
2021-04-10 12:38:17 +0200

    Initial Commit

---
+++ VImageBufferHelper/VImageBufferHelper.h
@@ -0,0 +1,18 @@
+//
+//  VImageBufferHelper.h
+//  VImageBufferHelper
+//
+//  Created by Alessandro Toschi on 10/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VImageBufferHelper.
+FOUNDATION_EXPORT double VImageBufferHelperVersionNumber;
+
+//! Project version string for VImageBufferHelper.
+FOUNDATION_EXPORT const unsigned char VImageBufferHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VImageBufferHelper/PublicHeader.h>
+
+
